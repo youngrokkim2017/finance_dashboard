cafd15707b0d2801dcf36d16e3779592d7eee9c7
Patrick Kim <patrick.yrkim@gmail.com>
2023-05-08 23:59:31 -0700

    Add mui themes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,16 @@
+import { useMemo } from "react"
+import { createTheme } from "@mui/material/styles"
+import { CssBaseline, ThemeProvider} from "@mui/material"
+import { themeSettings } from "./theme"
+
 function App() {
+  const theme = useMemo(() => createTheme(themeSettings), [])
+
   return (
     <div className="app">
-      
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
     </div>
   )
 }
