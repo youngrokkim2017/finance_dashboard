38c05c3ad69f3e5fd35301b7a0eb2aa3927e78c2
Patrick Kim <patrick.yrkim@gmail.com>
2023-05-09 22:49:35 -0700

    Add navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ import { createTheme } from "@mui/material/styles"
 import { Box, CssBaseline, ThemeProvider} from "@mui/material"
 import { themeSettings } from "./theme"
 
-// import Navbar from "@/scenes/navbar"
+import Navbar from "@/scenes/navbar"
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), [])
@@ -15,7 +15,7 @@ function App() {
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
-            {/* <Navbar /> */}
+            <Navbar />
             <Routes>
               <Route path="/" element={<div>dashboard page</div>} />
               <Route path="/predictions" element={<div>predictions page</div>} />
