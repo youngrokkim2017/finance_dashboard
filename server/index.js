f7fbbc7330f74e56cdd5fdeb82054d1361e7049e
Patrick Kim <patrick.yrkim@gmail.com>
2023-05-15 21:36:14 -0700

    Add mongoose connection

--- server/index.js
+++ server/index.js
@@ -16,3 +16,15 @@ app.use(morgan("common"))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors())
+
+// mongoose setup
+const PORT = process.env.PORT || 9000
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(async () => {
+        app.listen(PORT, () => console.log(`server port: ${PORT}`))
+    })
+    .catch((error) => console.log(`${error} did not connect`))
