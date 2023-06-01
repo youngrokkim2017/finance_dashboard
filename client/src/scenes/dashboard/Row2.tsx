88290e4fcf04001555950eeb8bffd10481d988d3
Patrick Kim <patrick.yrkim@gmail.com>
2023-05-31 23:58:12 -0700

    Add pie chart

--- client/src/scenes/dashboard/Row2.tsx
+++ client/src/scenes/dashboard/Row2.tsx
@@ -1,15 +1,20 @@
 import React, { useMemo } from 'react'
-import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
-import { useTheme } from '@mui/material'
+import { CartesianGrid, Cell, Legend, Line, LineChart, Pie, PieChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
+import { Box, Typography, useTheme } from '@mui/material'
 
 import DashboardBox from '@/components/DashboardBox'
 import { useGetKpisQuery, useGetProductsQuery } from '@/state/api'
 import BoxHeader from '@/components/BoxHeader'
+import FlexBetween from '@/components/FlexBetween'
 
-type Props = object
+const pieData = [
+  { name: "Group A", value: 600 },
+  { name: "Group B", value: 400 },
+]
 
-const Row2 = (props: Props) => {
+const Row2 = () => {
   const { palette } = useTheme()
+  const pieColors = [palette.primary[800], palette.primary[300]]
   const { data: operationalData } = useGetKpisQuery()
   const { data: productData } = useGetProductsQuery()
 
@@ -53,7 +58,45 @@ const Row2 = (props: Props) => {
           </LineChart>
         </ResponsiveContainer>
       </DashboardBox>
-      <DashboardBox gridArea="e"></DashboardBox>
+      <DashboardBox gridArea="e">
+        <BoxHeader title="campaigns and targets" sideText="+4%" />
+        <FlexBetween mt="0.25rem" gap="1.5rem" pr="1rem">
+          <PieChart 
+            width={110} 
+            height={100}
+            margin={{
+                top: 0,
+                right: -10,
+                left: 10,
+                bottom: 0,
+              }}
+          >
+            <Pie
+              stroke="none"
+              data={pieData}
+              innerRadius={18}
+              outerRadius={38}
+              paddingAngle={2}
+              dataKey="value"
+            >
+              {pieData.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={pieColors[index]} />
+              ))}
+            </Pie>
+          </PieChart>
+        </FlexBetween>
+        <Box ml="-0.7rem" flexBasis="40%" textAlign="center">
+          <Typography variant="h5">Target Sales</Typography>
+          <Typography variant="h3" m="0.3rem" color={palette.primary[300]}>81</Typography>
+          <Typography variant="h6">Finance goals of the campaign that is desired</Typography>
+        </Box>
+        <Box flexBasis="40%">
+          <Typography variant="h5">Loses in Revenue</Typography>
+          <Typography variant="h6">Loses are down 25%</Typography>
+          <Typography mt="0.4rem" variant="h5">Profit Margins</Typography>
+          <Typography variant="h6">Margins are up by 30% from last month</Typography>
+        </Box>
+      </DashboardBox>
       <DashboardBox gridArea="f"></DashboardBox>
     </>
   )
