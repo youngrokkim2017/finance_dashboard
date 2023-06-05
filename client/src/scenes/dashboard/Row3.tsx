7d1abc4af2f01930f29700e3a5815035f2aa3b23
Patrick Kim <patrick.yrkim@gmail.com>
2023-06-04 23:56:23 -0700

    Add data grid

--- client/src/scenes/dashboard/Row3.tsx
+++ client/src/scenes/dashboard/Row3.tsx
@@ -1,13 +1,63 @@
 import React from 'react'
+import { DataGrid, GridCellParams } from '@mui/x-data-grid'
+import { Box, useTheme } from '@mui/material'
 
 import DashboardBox from '@/components/DashboardBox'
+import { useGetKpisQuery, useGetProductsQuery, useGetTransactionsQuery } from '@/state/api'
+import BoxHeader from '@/components/BoxHeader'
 
 type Props = object
 
 const Row3 = (props: Props) => {
+  const { data: kpiData } = useGetKpisQuery()
+  const { data: productData } = useGetProductsQuery()
+  const { data: transactionData } = useGetTransactionsQuery()
+  const { palette } = useTheme()
+
+  const productColumns = [
+    {
+      field: "_id",
+      headerName: "id",
+      flex: 1,
+    },
+    {
+      field: "expense",
+      headerName: "Expense",
+      flex: 0.5,
+      renderCell: (params: GridCellParams) => `$${params.value}`,
+    },
+    {
+      field: "price",
+      headerName: "Price",
+      flex: 0.5,
+      renderCell: (params: GridCellParams) => `$${params.value}`,
+    },
+  ]
+
   return (
     <>
-      <DashboardBox gridArea="g"></DashboardBox>
+      <DashboardBox gridArea="g">
+        <BoxHeader 
+          title="List of Products"
+          sideText={`${productData?.length} products`}
+        />
+        <Box
+          mt="0.5rem"
+          p="0 0.5rem"
+          height="75%"
+          sx={{
+            "& .MuiDataGrid-root": {
+              color: palette.grey[300],
+              border: "none"
+            }
+          }}
+        >
+          <DataGrid
+            rows={productData || []}
+            columns={productColumns}
+          />
+        </Box>
+      </DashboardBox>
       <DashboardBox gridArea="h"></DashboardBox>
       <DashboardBox gridArea="i"></DashboardBox>
       <DashboardBox gridArea="j"></DashboardBox>
