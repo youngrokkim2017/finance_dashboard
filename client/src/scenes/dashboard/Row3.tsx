0e706d744f2e07574e2c591488e085c115870153
Patrick Kim <patrick.yrkim@gmail.com>
2023-06-05 23:34:09 -0700

    Add transactions grid

--- client/src/scenes/dashboard/Row3.tsx
+++ client/src/scenes/dashboard/Row3.tsx
@@ -34,6 +34,31 @@ const Row3 = (props: Props) => {
     },
   ]
 
+  const transactionColumns = [
+    {
+      field: "_id",
+      headerName: "id",
+      flex: 1,
+    },
+    {
+      field: "buyer",
+      headerName: "Buyer",
+      flex: 0.67,
+    },
+    {
+      field: "amount",
+      headerName: "Amount",
+      flex: 0.35,
+      renderCell: (params: GridCellParams) => `$${params.value}`,
+    },
+    {
+      field: "productIds",
+      headerName: "Count",
+      flex: 0.1,
+      renderCell: (params: GridCellParams) => (params.value as Array<string>).length,
+    },
+  ]
+
   return (
     <>
       <DashboardBox gridArea="g">
@@ -70,7 +95,40 @@ const Row3 = (props: Props) => {
           />
         </Box>
       </DashboardBox>
-      <DashboardBox gridArea="h"></DashboardBox>
+      <DashboardBox gridArea="h">
+      <BoxHeader 
+          title="Recent Orders"
+          sideText={`${transactionData?.length} latest transactions`}
+        />
+        <Box
+          mt="1rem"
+          p="0 0.5rem"
+          height="80%"
+          sx={{
+            "& .MuiDataGrid-root": {
+              color: palette.grey[300],
+              border: "none"
+            },
+            "& .MuiDataGrid-cell": {
+              borderBottom: `1px solid ${palette.grey[800]} !important`
+            },
+            "& .MuiDataGrid-columnHeaders": {
+              borderBottom: `1px solid ${palette.grey[800]} !important`
+            },
+            "& .MuiDataGrid-columnSeparator": {
+              visibility: "hidden",
+            },
+          }}
+        >
+          <DataGrid
+            columnHeaderHeight={25}
+            rowHeight={35}
+            hideFooter={true}
+            rows={transactionData || []}
+            columns={transactionColumns}
+          />
+        </Box>
+      </DashboardBox>
       <DashboardBox gridArea="i"></DashboardBox>
       <DashboardBox gridArea="j"></DashboardBox>
     </>
