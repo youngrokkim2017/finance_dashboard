c5ea53ade720b7bdc1be552dca3a671f04596b0d
Patrick Kim <patrick.yrkim@gmail.com>
2023-06-08 23:55:16 -0700

    Add box display data

--- client/src/scenes/dashboard/Row3.tsx
+++ client/src/scenes/dashboard/Row3.tsx
@@ -179,7 +179,26 @@ const Row3 = (props: Props) => {
           ))}
         </FlexBetween>
       </DashboardBox>
-      <DashboardBox gridArea="j"></DashboardBox>
+      <DashboardBox gridArea="j">
+        <BoxHeader title="Overall Summary and Explanation Data" sideText="+15%" />
+        <Box
+          height="15px"
+          margin="1.25rem 1rem 0.4rem 1rem"
+          bgcolor={palette.primary[800]}
+          borderRadius="1rem"
+        >
+          <Box
+            height="15px"
+            bgcolor={palette.primary[600]}
+            borderRadius="1rem"
+            width="40%"
+          >
+          </Box>
+        </Box>
+        <Typography margin="0 1rem" variant="h6">
+          lorem ipsum
+        </Typography>
+      </DashboardBox>
     </>
   )
 }
